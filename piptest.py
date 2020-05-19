8270f1f9600b8e9c9a67aae330565079a75585ed
Mark <a0938765923@gmail.com>
2020-05-19 18:26:21 +0800

    0519

--- piptest.py
+++ piptest.py
@@ -1,7 +1,7 @@
 import ntutrun
 import os
 retval = os.getcwd()
-ProjectPath="C:/Users/mark/Desktop/RobotTestThsis"
+ProjectPath="C:/Jenkins/workspace/mark_1"
 commandOPtions='-P ./ -P ./Project'
 # print(commandOPtions.split())
 ntutrun.exe(ProjectPath,outpath=retval, userXmlFileXpath="C:/Users/mark/Desktop/output.xml", precommitId="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613", commandOPt=commandOPtions, autoRun=False)
