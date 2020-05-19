b919991018f158591502c322a0879b5ed6a1c3cb
Mark <a0938765923@gmail.com>
2020-05-19 18:42:51 +0800

    0519

--- piptest.py
+++ piptest.py
@@ -4,4 +4,5 @@ retval = os.getcwd()
 ProjectPath="C:/Jenkins/workspace/mark_1"
 commandOPtions='-P ./ -P ./Project'
 # print(commandOPtions.split())
-ntutrun.exe(ProjectPath,outpath=retval, userXmlFileXpath="C:/Users/mark/Desktop/output.xml", precommitId="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613", commandOPt=commandOPtions, autoRun=False)
+# userXmlFileXpath="C:/Users/mark/Desktop/output.xml"
+ntutrun.exe(ProjectPath,outpath=retval, precommitId="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613", commandOPt=commandOPtions, autoRun=False)
