34bac8a3f08804e5614aebf1c48262c6f15f7ee3
Mark <a0938765923@gmail.com>
2020-05-21 11:10:39 +0800

    0521

--- piptest.py
+++ piptest.py
@@ -2,7 +2,8 @@ import ntutrun
 import os
 retval = os.getcwd()
 ProjectPath="C:/Jenkins/workspace/mark_1"
-commandOPtions='-P ./ -P ./Project'
-# print(commandOPtions.split())
-# userXmlFileXpath="C:/Users/mark/Desktop/output.xml"
-ntutrun.exe(ProjectPath,outpath=retval, precommitId="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613", commandOPt=commandOPtions, autoRun=True)
+commandOPtions="-P ./ -P ./Project"
+commitID="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613"
+commitID2="0fb60c0ec276108be1592059dc59afcbe2960e8b"
+ntutrun.exe(ProjectPath, outpath=retval, precommitId=commitID, cmpcommitId=commitID2,commandOPt=commandOPtions, autoRun=True)
+# ntutrun.exe(ProjectPath, outpath=retval, precommitId=commitID, commandOPt=commandOPtions, autoRun=True)
