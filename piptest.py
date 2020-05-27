dac6294205948d4f4f689259bac3c41236ec03be
Mark <a0938765923@gmail.com>
2020-05-27 17:57:25 +0800

    0527

--- piptest.py
+++ piptest.py
@@ -3,11 +3,9 @@ import os
 import sys
 
 arguments = len(sys.argv) - 1
-
 key=[]
 value=[]
 opt={}
-
 position = 1
 while (arguments >= position):
     if sys.argv[position].startswith("--"):
@@ -19,13 +17,7 @@ while (arguments >= position):
 for i in range(0,len(key)):
     opt[key[i]]=value[i]
 
-# print(a[1])
-# print(commandOPtions.split())
-# userXmlFileXpath="C:/Users/mark/Desktop/output.xml"
 retval = os.getcwd()
-path="C:/Jenkins/workspace/mark_1"
-commandOPtions='-P ./ -P ./Project'
-precommitId="ad9861b3f5c7ae65e1e91ae4f2c6bbb71b0ea613"
-
-# ntutrun.exe(ProjectPath=path, outpath=retval, commandOPt=commandOPtions, autoRun=True)
 ntutrun.exe(**opt, outpath=retval)
+
+# userXmlFileXpath="C:/Users/mark/Desktop/output.xml"
